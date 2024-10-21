1fb3b650f47bbe65dfda50cf8aa3c0ccbac5ccb4
Benjamin Schoolland <bschoolland@gmail.com>
2024-10-20 21:30:48 -0700

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BSchoolland's first package
+This repository is for my first ever PyPi package. It's a simple, useless package designed to help me learn how to create and publish packages to PyPi.
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bschoollands_first_package", 
+    version="0.1.0",
+    author="Benjamin Schoolland",
+    author_email="bschoolland@gmail.com",
+    description="Ben Schoolland's first ever Python package!",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="TBA",  # FIXME: Add the URL to my GitHub repository
+    packages=find_packages(), 
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'bschoollands_first_package=bschoollands_first_package:main',  # Format: 'command=module:function'
+        ],
+    },
+    python_requires=">=3.6", # not sure if this is necessary but it's in the example
+    install_requires=[
+        # Add your dependencies here (if any)
+    ],
+)
